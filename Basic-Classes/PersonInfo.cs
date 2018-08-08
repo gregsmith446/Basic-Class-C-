c189166274b6f92a5149e12bf1b1a48de72d38a3
Gregory Smith <gregsmith446@gmail.com>
2018-08-07 23:15:32 -0400

    Completed Walkthrough

---
+++ Basic-Classes/PersonInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Basic_Classes.Person
+{
+    public class Person1
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Basic-Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Basic_Classes.Math;
+using Basic_Classes.Person;
+
+namespace Basic_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greg = new Person1();
+            greg.FirstName = "Greg";
+            greg.LastName = "Smith";
+            greg.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
